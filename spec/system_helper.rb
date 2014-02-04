2b4b1434a1ce1cdc03b1f17426ea49b89adb19c6
Justin Stoller <justin@puppetlabs.com>
2014-02-04 15:23:49 -0800

    improve system testing

--- spec/system_helper.rb
+++ spec/system_helper.rb
@@ -1,80 +1,180 @@
-require 'beaker-rspec/beaker_shim'
-include BeakerRSpec::BeakerShim
+require 'beaker'
 
-ROOT       = File.expand_path(File.join(__FILE__, '..', '..'))
-support    = File.join(ROOT, 'spec', 'support')
-modname    = File.basename(ROOT).split('-', 2).pop
-hosts      = ENV['SYSTEM_HOSTS'] || 'default'
-hosts_file = File.join(support, 'hosts', hosts + '.yml')
+module Beaker
+  module RSpec
+    module Bridge
+      def hosts
+        @hosts ||= Beaker::RSpec::TestState.instance.hosts.dup
+      end
 
-destroy   = ENV['SPEC_DESTROY'] == 'true'   ? '' : '--preserve-hosts'
-provis    = ENV['SPEC_PROVISION'] == 'true' ? '' : '--no-provision'
-keyfile   = ENV['SPEC_KEYFILE']             ? ['--keyfile', ENV['SPEC_KEYFILE']] : ['--keyfile', File.join(support, 'insecure_private_key')]
-debug     = ENV['SPEC_DEBUG']               ? ['--log-level', 'debug']           : []
+      def options
+        @options ||= Beaker::RSpec::TestState.instance.options.dup
+      end
 
+      def logger
+        @logger ||= options[:logger]
+      end
+    end
 
+    # This manages our test state / beaker run stages in an RSpec aware way
+    class TestState
+      require 'pathname'
+      require 'singleton'
 
+      include Singleton
 
-#Beaker::Utils::Validator.validate(@hosts, @options[:logger])
+      attr_reader :rspec_config, :options, :logger, :network_manager, :hosts, :node_file
 
+      def hunt_for_file(bare_file, rspec_config)
+        yml_file = bare_file + '.yml'
 
-RSpec.configure do |c|
+        possibilities = [bare_file, yml_file].map do |basename|
+          spec_dir        = Pathname(rspec_config.default_path)
+          in_cwd          = File.expand_path( basename )
+          in_project_root = File.join(spec_dir.parent, basename)
+          in_spec_dir     = File.join(spec_dir, basename)
+          in_support_dir  = File.join(spec_dir, 'support', basename)
+          in_nodes_dir    = File.join(spec_dir, 'support', 'nodes', basename)
 
-  if STDOUT.tty?
-    # Enable color
-    c.tty = true
-  end
+          [ in_cwd, in_project_root, in_spec_dir,
+            in_support_dir, in_nodes_dir          ].find do |file|
 
-  # Define persistant hosts setting
-  c.add_setting :hosts, :default => []
-  # Define persistant options setting
-  c.add_setting :options, :default => {}
+            File.exists?( file )
+          end
+        end
 
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # gather our opts
-    args = [destroy, provis, '--hosts', hosts_file, keyfile, debug].flatten
-    options_parser = Beaker::Options::Parser.new
-    @options = options_parser.parse_args(args)
-    @options[:debug] = true
-    logger = Beaker::Logger.new(options)
-    @options[:logger] = logger
+        return possibilities.flatten.compact.first
+      end
 
-    # creat the netman
-    @network_manager = Beaker::NetworkManager.new(@options, @options[:logger])
+      def configure!(rspec_config)
+        @rspec_config = rspec_config
 
-    # set test variables
-    c.options = @options
-    c.hosts   = @network_manager.provision
+        defaults   = Beaker::Options::Presets.presets
+        env_opts   = Beaker::Options::Presets.env_vars
+        @node_file = hunt_for_file(rspec_config.node_set, rspec_config)
+        this_run_dir = File.join('.vagrant', 'beaker_vagrant_files', File.basename(@node_file))
+        provisioned = File.exists?(this_run_dir)
+        rspec_config.provision = provisioned ? rspec_config.provision : true
+        node_opts  = Beaker::Options::HostsFileParser.parse_hosts_file(node_file)
+        user_opts  = rspec_config.beaker.merge({
+                       :color      => rspec_config.color,
+                       :log_level  => 'debug',
+                       :quiet      => false,
+                       :hosts_file => File.basename(node_file),
+                       :provision  => rspec_config.provision
+        })
 
-    # prepare our env for the test suite
-    c.hosts.each do |host|
-      mod_on_host = "#{host['distmoduledir']}/#{modname}"
-      on( host, "mkdir -p #{mod_on_host}" )
+        @options  = defaults.
+                      merge(node_opts).
+                      merge(env_opts).
+                      merge(user_opts)
 
-      on( host, 'gem install rake bundler --no-ri --no-rdoc' )
-      on( host, "mkdir -p #{mod_on_host}" )
-      %w{Modulefile manifests templates files
-         Puppetfile Gemfile
-         tasks Rakefile}.each do |to_trans|
+        key_file  = hunt_for_file(rspec_config.ssh_key, rspec_config)
+        @options[:ssh][:keys] = [File.expand_path(key_file)]   # Grrr...
 
-        local_file = File.join(ROOT, to_trans)
+        @logger   = Beaker::Logger.new( options )
 
-        if File.exists?( local_file )
-          scp_to( host, local_file, "#{mod_on_host}/#{to_trans}")
+        @options[:logger] = logger
+
+        @network_manager = Beaker::NetworkManager.new(options, options[:logger])
+        @hosts = options['HOSTS'].map do |hostname, info|
+          Beaker::Host.create(hostname, options)
         end
       end
 
-      on( host, "puppet apply #{mod_on_host}/manifests/prerequisites/dev.pp")
-      on( host,
-          "cd #{mod_on_host}; " +
-          "BUNDLE_WITHOUT='ci lint spec pkg' rake deps:ruby; " +
-          "LIBRARIAN_PUPPET_PATH=#{host['distmoduledir']} rake deps:puppet")
+      def validate!
+        opts = {:color => rspec_config.color}
+        Beaker::Utils::Validator.validate(hosts, options[:logger])
+      end
+
+      def provision!
+        @hosts = network_manager.provision
+      end
+
+      def destroy!
+        network_manager.cleanup
+      end
+
+      def default_setup_steps
+        # prepare our env for the test suite
+        hosts = Beaker::RSpec::TestState.instance.hosts
+        default_host = hosts.find do |host|
+          ['default', :default, 'master'].any? do |role|
+            host['roles'].include?( role )
+          end
+        end
+        root = Pathname(rspec_config.default_path).parent.realpath
+        modname = root.basename.to_s.split('-', 2).pop
+        mod_on_node = "#{default_host['distmoduledir']}/#{modname}"
+        default_host.exec(Beaker::Command.new( "mkdir -p #{mod_on_node}" ))
+
+        %w{Modulefile manifests templates files
+           Puppetfile Gemfile
+           tasks Rakefile}.each do |to_trans|
+
+          local_file = File.join(root.to_s, to_trans)
+
+          if File.exists?( local_file )
+            default_host.do_scp_to( local_file, "#{mod_on_node}/#{to_trans}", {})
+          end
+        end
+
+        if File.exists?(File.join(root.to_s, *(Array(rspec_config.setup_manifest).flatten)))
+          default_host.exec(Beaker::Command.new("puppet apply #{mod_on_node}/manifests/prerequisites/dev.pp"))
+        end
+
+        default_host.exec(Beaker::Command.new( "cd #{mod_on_node}; " +
+               "BUNDLE_WITHOUT='ci lint spec pkg' rake deps:ruby; " +
+               "LIBRARIAN_PUPPET_PATH=#{default_host['distmoduledir']} rake deps:puppet"))
+      end
+    end
+  end
+end
+
+# I hate this, here we set up a prettier way to configure beaker via RSpec
+::RSpec.configure do |c|
+  c.add_setting :node_set,       :default => ENV['SPEC_NODES'] || 'default.yml'
+  c.add_setting :provision,      :default => ENV['SPEC_PROVISION'] == 'true'
+  c.add_setting :validate,       :default => ENV['SPEC_VALIDATE'] == 'true'
+  c.add_setting :destroy,        :default => ENV['SPEC_DESTROY'] == 'true'
+  c.add_setting :ssh_key,        :default => ENV['SPEC_KEYFILE'] || 'insecure_private_key'
+  c.add_setting :beaker,         :default => Hash.new
+  c.add_setting :setup_steps,    :default => nil
+  c.add_setting :setup_manifest, :default => ['manifests', 'prerequisites', 'dev.pp']
+end
+
+# Here we inject Beaker's default stages into our RSpec test run
+::RSpec.configure do |c|
+  c.before :suite do
+    # Why yes, I do want to pass around a Singleton as an arg (to another singleton)
+    Beaker::RSpec::TestState.instance.configure!(::RSpec.configuration)
+
+    # We have to call `provision!` regardless of whether or not we're
+    # really provisioning because it's in this step that the old ip
+    # (if it exists) is found
+    Beaker::RSpec::TestState.instance.provision!
+
+    if ::RSpec.configuration.validate
+      Beaker::RSpec::TestState.instance.validate!
+    end
+
+    if ::RSpec.configuration.provision
+      Beaker::RSpec::TestState.instance.default_setup_steps
     end
   end
 
-  #Destroy nodes if no preserve hosts
-  #c.after :suite do
-  #  @network_manager.cleanup
-  #end
+  c.after :suite do
+    Beaker::RSpec::TestState.instance.destroy! if ::RSpec.configuration.destroy
+  end
 end
+
+# This is the minimum needed in an RSpec example to allow using the Beaker DSL
+# This is the same thing that would happen if you set:
+#   let(:hosts) { Beaker::RSpec::TestState.instance.hosts.dup }
+#   ...etc...
+# within your tests
+::RSpec.configure do |c|
+  c.include Beaker::DSL
+  c.include Beaker::RSpec::Bridge
+end
+
